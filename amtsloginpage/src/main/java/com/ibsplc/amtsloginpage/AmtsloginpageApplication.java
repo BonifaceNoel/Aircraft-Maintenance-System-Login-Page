e75a04e9fe3a2fc73399f7ebb6c26680ab0eb1d1
BonifaceNoel <bonifacemac658@gmail.com>
2023-12-04 15:52:26 +0530

    update-gitignore-upload-initail-files

---
+++ amtsloginpage/src/main/java/com/ibsplc/amtsloginpage/AmtsloginpageApplication.java
@@ -0,0 +1,13 @@
+package com.ibsplc.amtsloginpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmtsloginpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmtsloginpageApplication.class, args);
+	}
+
+}
