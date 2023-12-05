a2833af2289da3ff69c7199010a2d56d9ee928b6
BonifaceNoel <bonifacemac658@gmail.com>
2023-12-05 17:26:59 +0530

    file-changes-from-security-config

--- amtsloginpage/src/main/java/com/ibsplc/amtsloginpage/controller/LoginController.java
+++ amtsloginpage/src/main/java/com/ibsplc/amtsloginpage/controller/LoginController.java
@@ -18,34 +18,4 @@ import com.ibsplc.amtsloginpage.util.JwtUtil;
 @RestController
 public class LoginController {
 
-	@Autowired
-	private AuthenticationManager authManager;
-
-	@Autowired
-	private LoginService loginService;
-
-	@Autowired
-	private JwtUtil jwtUtil;
-
-	@PostMapping("/login")
-	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authRequest) throws Exception{
-		try {
-			authManager.authenticate(
-					new UsernamePasswordAuthenticationToken(
-							authRequest.getUsername(),
-							authRequest.getUsername()
-							)
-					);
-		}
-
-		catch(BadCredentialsException ec) {
-			throw new Exception("Incorrect Username or Password", ec);
-		}
-
-		final UserDetails userDetails = loginService.loadUserByLoginName(authRequest.getUsername());
-
-		final String token = jwtUtil.createToken(userDetails);
-
-		return ResponseEntity.ok(new AuthenticationResponse(token));
-	}
 }
