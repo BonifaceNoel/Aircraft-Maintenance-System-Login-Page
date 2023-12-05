668838899b7ce99df7dd71c61a655fe6c0af5653
BonifaceNoel <bonifacemac658@gmail.com>
2023-12-05 10:40:26 +0530

    service-impl-layer-user-core-fix

--- amtsloginpage/src/main/java/com/ibsplc/amtsloginpage/service/LoginServiceImpl.java
+++ amtsloginpage/src/main/java/com/ibsplc/amtsloginpage/service/LoginServiceImpl.java
@@ -1,21 +1,26 @@
 package com.ibsplc.amtsloginpage.service;
 
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
 import com.ibsplc.amtsloginpage.bo.LoginUser;
 import com.ibsplc.amtsloginpage.exceptions.NoLoginNameException;
 import com.ibsplc.amtsloginpage.repository.LoginRepository;
 
-public class LoginServiceImpl implements LoginService{
+@Service
+public class LoginServiceImpl implements LoginService {
 
 	@Override
 	public UserDetails loadUserByLoginName(String loginname) throws NoLoginNameException {
 		LoginUser user = LoginRepository.findByLoginName(loginname);
 
-		if(user == null) {
+		if (user == null) {
 			throw new NoLoginNameException("Username not found: ", loginname);
 		}
-		return null;
+		return new User(user.getLoginName(), user.getLoginPassword(), new ArrayList<>());
 	}
 
 }
