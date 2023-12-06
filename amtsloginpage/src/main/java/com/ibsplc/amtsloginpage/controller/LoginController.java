ea4f764d5566f4ee5273a8f3ad7b869658e45bf1
BonifaceNoel <bonifacemac658@gmail.com>
2023-12-06 10:23:02 +0530

    login-controller-get-user-access

--- amtsloginpage/src/main/java/com/ibsplc/amtsloginpage/controller/LoginController.java
+++ amtsloginpage/src/main/java/com/ibsplc/amtsloginpage/controller/LoginController.java
@@ -1,8 +1,39 @@
 package com.ibsplc.amtsloginpage.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ibsplc.amtsloginpage.bo.LoginUser;
+import com.ibsplc.amtsloginpage.exceptions.NoLoginNameException;
+import com.ibsplc.amtsloginpage.service.LoginService;
+
 @RestController
+@CrossOrigin(origins = "https://localhost:5173")
 public class LoginController {
 
+	@Autowired
+	LoginService logService;
+
+	@GetMapping(value="/login", produces=MediaType.APPLICATION_JSON_VALUE)
+	private ResponseEntity<String> getLoginInfo(String userName) throws NoLoginNameException{
+		ResponseEntity<String> loginInfo = null;
+		LoginUser user = logService.loadUserByLoginName(userName);
+
+		String responseMessage = null;
+
+		if(user != null) {
+			responseMessage = "{\"Login User found\" : \"Logged in\"}";
+			loginInfo = new ResponseEntity<>(responseMessage, HttpStatus.ACCEPTED);
+		}
+		else {
+			responseMessage = "{\"No User found\" : \"No Access\"}";
+			loginInfo = new ResponseEntity<>(responseMessage, HttpStatus.NOT_ACCEPTABLE);
+		}
+		return loginInfo;
+	}
 }
