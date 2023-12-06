78c3b6b6ae01d74490246d0ac9437abbbdccad4c
BonifaceNoel <bonifacemac658@gmail.com>
2023-12-06 12:37:49 +0530

    login-service-update-new-user

--- amtsloginpage/src/main/java/com/ibsplc/amtsloginpage/service/LoginService.java
+++ amtsloginpage/src/main/java/com/ibsplc/amtsloginpage/service/LoginService.java
@@ -3,10 +3,13 @@ package com.ibsplc.amtsloginpage.service;
 import org.springframework.stereotype.Service;
 
 import com.ibsplc.amtsloginpage.bo.LoginUser;
+import com.ibsplc.amtsloginpage.exceptions.NewUserInvalidException;
 import com.ibsplc.amtsloginpage.exceptions.NoLoginNameException;
 
 @Service
 public interface LoginService {
 
 	public LoginUser loadUserByLoginName(String loginname) throws NoLoginNameException;
+	public void loadNewUser(LoginUser newUser) throws NewUserInvalidException;
+
 }
