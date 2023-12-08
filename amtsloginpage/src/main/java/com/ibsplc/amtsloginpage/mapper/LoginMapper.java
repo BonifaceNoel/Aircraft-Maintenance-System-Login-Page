2ce6c468c5d3e59e37cd767278c7e783c382e7cb
BonifaceNoel <bonifacemac658@gmail.com>
2023-12-08 17:02:11 +0530

    mapper-update-for-access-key-retreive

--- amtsloginpage/src/main/java/com/ibsplc/amtsloginpage/mapper/LoginMapper.java
+++ amtsloginpage/src/main/java/com/ibsplc/amtsloginpage/mapper/LoginMapper.java
@@ -1,5 +1,7 @@
 package com.ibsplc.amtsloginpage.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -13,9 +15,14 @@ public interface LoginMapper{
 	@Select("SELECT * FROM `aircraft_maintenance_system`.`login_credentials` WHERE login_name = #{name};")
 	LoginUser getByUserName(@Param("name") String loginName);
 
+	@Select("SELECT access_key FROM `aircraft_maintenance_system`.`login_credentials` WHERE login_name = #{name};")
+	String getByAccessKey(@Param("name") String loginName);
+
 	@Insert("INSERT INTO `aircraft_maintenance_system`.`login_credentials` \r\n"
-			+ "  (`login_name`, `login_role`, `login_password`, `access_key`) \r\n"
+			+ "  (`login_name`, `login_role`, `access_key`) \r\n"
 			+ "VALUES \r\n"
-			+ "  (#{logname}, #{logrole}, #{logpass}, #{acckey})")
-	void newUserRegist(@Param("logname") String logName, @Param("logpass") String logPass, @Param("logrole") String logRole, @Param("acckey") String accKey);
+			+ "  (#{logname}, #{logrole}, #{acckey})")
+	void newUserRegist(@Param("logname") String logName, @Param("logrole") String logRole, @Param("acckey") String accKey);
+
+	List<String> selectAllAccessTokens();
 }
